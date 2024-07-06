ffc5aeb42803f1b620a32d011554615617bf1765
Christopher Handy <handyc@helxt.com>
2024-07-06 22:57:30 +0200

    Update 1is1.py
    condensed

--- 1is1.py
+++ 1is1.py
@@ -1,29 +1,22 @@
-version="1is1 interpreter 0.3"
-print(version)
-
+version="1is1 interpreter 0.4"; print(version)
 def f1is1resolve(x):
     print(x)
-
 def f1is1browser(x):
     print(x)
-
 def f1is1input(x):
     print(x)
-
 def f1is1output(x):
     print(x)
-
-string = ""
-prompt = "hello: "
+def f16(x):
+    print(x)
+string = ""; prompt = "hello: "
 while(string != 'quit()'):
     string = input(prompt)
     if "=" in string:
-        equality=string.split("=")
-        print("Creating new variables: ", end="")
+        equality=string.split("="); print("Creating new variables: ", end="")
         for variable in equality:
             print(variable.strip() + ", ", end="")
         print("")
     else:
         print(string)
-print("1is1")
-print("goodbye")
+print("1is1"); print("goodbye")

45f0eb673b81a61ebed7857d2073aee936d43e1d
Christopher Handy <handyc@helxt.com>
2024-07-06 23:06:42 +0200

    Update 1is1.py
    imports

--- 1is1.py
+++ 1is1.py
@@ -1,4 +1,5 @@
-version="1is1 interpreter 0.4"; print(version)
+import os, sys
+version="1is1 interpreter 0.5"; print(version)
 def f1is1resolve(x):
     print(x)
 def f1is1browser(x):
@@ -9,14 +10,13 @@ def f1is1output(x):
     print(x)
 def f16(x):
     print(x)
-string = ""; prompt = "hello: "
+string = ""; prompt = "\nhello: "
 while(string != 'quit()'):
     string = input(prompt)
     if "=" in string:
         equality=string.split("="); print("Creating new variables: ", end="")
         for variable in equality:
             print(variable.strip() + ", ", end="")
-        print("")
     else:
         print(string)
 print("1is1"); print("goodbye")

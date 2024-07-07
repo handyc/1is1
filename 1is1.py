92101556bfc6db5f54d617a2c00dfef7b18b533d
Christopher Handy <handyc@helxt.com>
2024-07-07 16:04:02 +0200

    Update 1is1.py
    resolution improvement

--- 1is1.py
+++ 1is1.py
@@ -1,31 +1,38 @@
 import os, sys
-version="1is1 interpreter 0.73"; print(version)
+version="1is1 interpreter 0.74"; print(version)
 stack={} # allocate 1is1 stack
 
 def f1is1resolve(x):
     if('"' in x):
         newstring=(x.split('"')[1])
         stack.update({"message":newstring})
+        print("1")
     if("input" in x):
         xe=x.split("input("); xo=xe[1].split(")"); f1is1input(xo[0])
+        print("2")
     if("output" in x):
         xe=x.split("output("); xo=xe[1].split(")");
         #print(stack[(xo[0])])
         outmsg=(stack[(xo[0])])
+        print(outmsg)
         f1is1output(outmsg)
+        print("3")
     if("resolve" in x):
         xe=x.split("resolve("); xo=xe[1].split(")"); f1is1resolve(xo[0])
+        print("4")
     if("browser" in x):
         print(x, end="")
+        print("5")
     if("f16" in x):
         print(x, end="")
+        print("6")
 def f1is1browser(x):
     print(x)
 def f1is1input(prompt):
     string = input(prompt)
     return string
-def f1is1output(x):
-    print(x)
+def f1is1output(outmsg):
+    print(outmsg)
 def f16(x):
     print(x)
 dictionary = {}; string = ""; prompt = "\nhello: "
@@ -41,14 +48,20 @@ else:
             entry = {}
             equality=string.split("="); print("Creating variables: ", end="")
             neighbor=""
+            head=equality[0]
             for variable in equality:
                 stripped = variable.strip()
                 entry.update({stripped:neighbor})
                 print(stripped + ", ", end="")
                 neighbor=stripped
+                stack.update({head:entry})
+            #stack.update({string:entry})
             dictionary.update({string:entry})
+            f1is1resolve(string)
+
         else:
             print(string)
+print(stack);
 print(dictionary);
 for entry in dictionary:
     print(entry + " : " + str(dictionary[entry]))

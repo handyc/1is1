4ae18444cd3dd7b6cbd482a7e62927345b538d1f
Christopher Handy <handyc@helxt.com>
2024-07-07 23:56:08 +0200

    Update 1is1.py
    moving things around

--- 1is1.py
+++ 1is1.py
@@ -1,14 +1,34 @@
 import os, sys
-version="1is1 interpreter 0.741"; print(version)
+version="1is1 interpreter 0.742"; print(version)
 stack={} # allocate 1is1 stack
 
 def f1is1output(outmsg):
     print(outmsg)
 
 def f1is1resolve(x):
+    if "=" in x:
+        entry = {}
+        equality=x.split("=");
+        leftside=equality[0]
+        rightside=equality[1]
+        stack.update({leftside.strip():rightside.strip()})
+        neighbor=""
+        head=equality[0]
+        #print("stack: " + str(stack))
+        #print("\nCreating variables: ", end="")
+        for variable in equality:
+            stripped = variable.strip()
+            entry.update({stripped:neighbor})
+            #print(stripped + ", ", end="")
+            neighbor=stripped
+            stack.update({head.strip():entry})
+        stack.update({x.strip():entry})
+        #dictionary.update({x:entry})
+    else:
+        print(string)
     if('"' in x):
         newstring=(x.split('"')[1])
-        stack.update({"message":newstring})
+        stack.update({leftside:newstring})
         #print("1")
     if("input" in x):
         xe=x.split("input("); xo=xe[1].split(")"); f1is1input(xo[0])
@@ -19,12 +39,14 @@ def f1is1resolve(x):
             newstring=(xo.split('"')[1])
             stack.update({xo:newstring})
         #print(stack[(xo[0])])
-        if not xo[0] in stack:
-            stack.update({xo[0]:xo[0]})
-        outmsg=(stack[(xo[0])])
-        print(outmsg)
+        #if not xo[0] in stack:
+        #    stack.update({xo[0]:rightside})
+        #outmsg=(stack.items())
+        outmsg=(stack[xo[0]])
+        #outmsg=(stack[xo[stack[xo[0]]]])
+        #print(outmsg)
         f1is1output(outmsg)
-        print("3")
+        #print("3")
     if("resolve" in x):
         xe=x.split("resolve("); xo=xe[1].split(")"); f1is1resolve(xo[0])
         print("4")
@@ -41,7 +63,10 @@ def f1is1input(prompt):
     return string
 def f16(x):
     print(x)
-dictionary = {}; string = ""; prompt = "\nhello: "
+
+#dictionary = {};
+string = ""
+prompt = "\nhello: "
 if len(sys.argv) > 1:
     filename = sys.argv[1]
     f = open(filename, "r"); r = f.readlines(); f.close()
@@ -50,24 +75,14 @@ if len(sys.argv) > 1:
 else:
     while(string != 'quit()'):
         string = input(prompt)
-        if "=" in string:
-            entry = {}
-            equality=string.split("="); print("Creating variables: ", end="")
-            neighbor=""
-            head=equality[0]
-            for variable in equality:
-                stripped = variable.strip()
-                entry.update({stripped:neighbor})
-                print(stripped + ", ", end="")
-                neighbor=stripped
-                stack.update({head:entry})
-            stack.update({string:entry})
-            dictionary.update({string:entry})
-            f1is1resolve(string)
-        else:
-            print(string)
-print(stack);
-print(dictionary);
-for entry in dictionary:
-    print(entry + " : " + str(dictionary[entry]))
-print("1is1"); print("Goodbye!")
+        f1is1resolve(string)
+
+#end
+#print(stack);
+#for entry in stack:
+#    print(entry + " : " + str(stack[entry]))
+
+#print(dictionary);
+#for entry in dictionary:
+#    print(entry + " : " + str(dictionary[entry]))
+#print("1is1"); print("Goodbye!")
